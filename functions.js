5504c0d5fdb9db0f570a586813580869d5e2214c
Elias Lima <eliaslima@acad.ifma.edu.br>
2022-10-04 10:30:31 -0300

    criando-linha

--- functions.js
+++ functions.js
@@ -29,6 +29,52 @@ function zerarAssociacoes(){
    figuraInicio = 'nenhuma'
 }
 
+function criarLinha(elemento1, elemento2){
+  var line = document.createElement("div")
+  line.classList.add("ligacao")
+  var area = document.getElementById("areaFiguras")
+  area.appendChild(line)
+  ajustarLinha(elemento1, elemento2, line)
+}
+
+function ajustarLinha(from, to, line){
+
+  var fT = from.offsetTop  + from.offsetHeight/2;
+  var tT = to.offsetTop    + to.offsetHeight/2;
+  var fL = from.offsetLeft + from.offsetWidth/2;
+  var tL = to.offsetLeft   + to.offsetWidth/2;
+  
+  var CA   = Math.abs(tT - fT);
+  var CO   = Math.abs(tL - fL);
+  var H    = Math.sqrt(CA*CA + CO*CO);
+  var ANG  = 180 / Math.PI * Math.acos( CA/H );
+
+  if(tT > fT){
+      var top  = (tT-fT)/2 + fT;
+  }else{
+      var top  = (fT-tT)/2 + tT;
+  }
+  if(tL > fL){
+      var left = (tL-fL)/2 + fL;
+  }else{
+      var left = (fL-tL)/2 + tL;
+  }
+
+  if(( fT < tT && fL < tL) || ( tT < fT && tL < fL) || (fT > tT && fL > tL) || (tT > fT && tL > fL)){
+    ANG *= -1;
+  }
+  top-= H/2;
+
+  line.style["-webkit-transform"] = 'rotate('+ ANG +'deg)';
+  line.style["-moz-transform"] = 'rotate('+ ANG +'deg)';
+  line.style["-ms-transform"] = 'rotate('+ ANG +'deg)';
+  line.style["-o-transform"] = 'rotate('+ ANG +'deg)';
+  line.style["-transform"] = 'rotate('+ ANG +'deg)';
+  line.style.top    = top+'px';
+  line.style.left   = left+'px';
+  line.style.height = H + 'px';
+}
+
 function clickElemento(elemento){
 
   var Evento = function (elemento) {
@@ -49,6 +95,7 @@ function clickElemento(elemento){
         figuraInicio = elemento
       }else if(figuraInicio != elemento){
         fluxograma[fluxograma.length] = {figuraDoInicio: figuraInicio, figuraDoFim: elemento}
+        criarLinha(figuraInicio, elemento)
         figuraInicio = 'nenhuma'
         console.log(fluxograma)
       }
